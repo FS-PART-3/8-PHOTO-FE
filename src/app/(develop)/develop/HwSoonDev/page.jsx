59396e6519c6ec6044f9b2669c0a56bd1de6abcf
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-10-27 18:54:50 +0900

    유저 프로필, 랜덤상자 모달 UI 개발

--- src/app/(develop)/develop/HwSoonDev/page.jsx
+++ src/app/(develop)/develop/HwSoonDev/page.jsx
@@ -1,10 +1,16 @@
+import Profile from './Profile/Profile';
+import RandomBoxModal from './RandomBox/RandomBoxModal';
+
 // HwSoonDev 개발자 컴포넌트 테스트 페이지
 export default function HwSoonDevPage() {
   return (
     <div className="developer-page">
       <h2>HwSoonDev 개발자 페이지</h2>
       <p>컴포넌트 테스트 및 개발 작업을 위한 페이지입니다.</p>
-      {/* 개발자가 작업할 컴포넌트들이 여기에 추가될 예정 */}
+      <div className="flex justify-end">
+        <Profile />
+      </div>
+      <RandomBoxModal />
     </div>
   );
 }
