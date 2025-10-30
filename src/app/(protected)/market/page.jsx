afbba18800b3e42b5b704e282dc716aeb0f979f0
kmjerror <6945830a@gmail.com>
2025-10-31 03:29:47 +0900

    feat: 마켓플레이스 페이지 & 공통컴포넌트 모달, 타이틀

--- src/app/(protected)/market/page.jsx
+++ src/app/(protected)/market/page.jsx
@@ -1,10 +1,24 @@
 import Header from '@/components/organisms/header/Header';
+import Title from '@/components/molecules/Title';
 import MarketPage from '@/components/pages/MarketPage';
 
 export default function Page() {
   return (
     <>
       <Header />
+      <div className="mt-[60px] mx-auto max-w-[1200px]">
+        <Title
+          text="마켓플레이스"
+          size="48"
+          family="secondary"
+          weight={400}
+          containerWidth="full"
+          justify="between"
+          align="center"
+          action={{ label: '내 포토카드 판매하기', href: '/sell', size: 'auto', variant: 'primary' }}
+        />
+      </div>
+
       <MarketPage />
     </>
   );
