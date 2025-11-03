05f289d584bc8f97d252664ffa81c7d83005ee13
DESKTOP-EAN86Q6\vb120 <vb1201@naver.com>
2025-11-03 13:54:53 +0900

    feat/소셜 로그인 버튼

--- src/components/molecules/socialLogin/SocialLogin.jsx
+++ src/components/molecules/socialLogin/SocialLogin.jsx
@@ -1,11 +1,18 @@
 import Image from 'next/image';
 import googleImage from './google-logo.png';
+import Link from 'next/link';
 
 export default function SocialLogin({}) {
   return (
-    <button className="flex min-h-[60px] w-full max-w-[520px] cursor-pointer items-center justify-center gap-4 rounded-[2px] bg-[#d4d4aeff] text-[18px] font-bold text-[var(--color-black)] transition duration-200 ease-in-out hover:opacity-90">
-      간편 로그인 하기
-      <Image src={googleImage} className="h-[50px] w-[50px]" />
-    </button>
+    <Link
+      href={`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/google`}
+      className="relative flex min-h-[60px] w-full max-w-[520px] cursor-pointer items-center justify-center gap-4 rounded-[2px] bg-[#d4d4aeff] text-[18px] font-bold text-[var(--color-black)] transition duration-200 ease-in-out hover:opacity-90"
+    >
+      간편 로그인 (구글)
+      <Image
+        src={googleImage}
+        className="absolute right-20 h-[50px] w-[50px]"
+      />
+    </Link>
   );
 }
