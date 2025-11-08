c83cf7ddc46781c2152fb4937a96db8733836ac1
kwangche <dlrhkdcp11@gmail.com>
2025-11-08 17:06:35 +0900

    fix: 머지충돌 메세지 제거

--- src/state/useNotificationQuery.js
+++ src/state/useNotificationQuery.js
@@ -7,21 +7,11 @@ import { notificationService } from '@/services/notificationService';
 import QUERY_KEYS from '@/constants/queryKeys';
 
 /**
-<<<<<<< HEAD
- * 알림 목록 조회 훅
-=======
  * 알림 목록 조회 훅 (무한 스크롤 - cursor 기반)
->>>>>>> 8d993f85242ba90a28ea097d9af409bf98e303fa
  * @param {Object} options - 쿼리 옵션
  * @returns {Promise} API 응답
  */
 export const useNotificationList = (options = {}) =>
-<<<<<<< HEAD
-  useQuery({
-    queryKey: ['notification-list'],
-    queryFn: () => getNotificationList(),
-    enabled: true,
-=======
   useInfiniteQuery({
     queryKey: [QUERY_KEYS.NOTIFICATION_LIST],
     queryFn: ({ pageParam }) =>
@@ -36,7 +26,6 @@ export const useNotificationList = (options = {}) =>
         ? lastPage.pagination.nextCursor
         : undefined,
     initialPageParam: undefined,
->>>>>>> 8d993f85242ba90a28ea097d9af409bf98e303fa
     staleTime: 1000 * 30, // 30초
     refetchInterval: 1000 * 30, // 30초
     refetchIntervalInBackground: false, // 탭이 백그라운드에 있을 때는 리패치하지 않음
@@ -52,9 +41,6 @@ export const useReadNotification = notificationId => {
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEYS.NOTIFICATION_LIST],
       });
-    },
-    onError: error => {
-      console.error(error);
     },
   });
 };
