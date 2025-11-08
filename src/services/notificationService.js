816be796d3c2eebc2fb276a2227fbaa33f38bc22
kwangche <dlrhkdcp11@gmail.com>
2025-11-08 17:10:05 +0900

    fix: 알림 머지충돌 메세지 제거

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -1,15 +1,5 @@
 import fetchClient from '@/lib/fetchClient';
 
-<<<<<<< HEAD
-export const getNotificationList = async () => {
-  try {
-    const result = await fetchClient.authGet('/api/notifications');
-    return result;
-  } catch (error) {
-    console.error('알림 목록 조회 중 오류가 발생했습니다.');
-    throw error;
-  }
-=======
 export const notificationService = {
   getNotificationList({ cursor, limit = 5 } = {}) {
     const url = cursor
@@ -25,5 +15,4 @@ export const notificationService = {
       },
     });
   },
->>>>>>> 8d993f85242ba90a28ea097d9af409bf98e303fa
 };
