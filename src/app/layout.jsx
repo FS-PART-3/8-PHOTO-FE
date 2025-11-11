20da05d8cfa7261ef11a7c0400d3bc2fd46c7c80
seongEun95 <dsa02070@naver.com>
2025-11-11 16:07:02 +0900

    feat: open graph 속성추가, 랜딩페이지 SSG방식 적용 (use clinet 삭제)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,6 +5,11 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 export const metadata = {
   title: '최애의 포토',
   description: '개인용 디지털 사진첩 생성 플랫폼, 최애의 포토',
+  openGraph: {
+    title: '최애의 포토',
+    description: '개인용 디지털 사진첩 생성 플랫폼, 최애의 포토',
+    images: ['/assets/images/market-og.png'],
+  },
 };
 
 export default function RootLayout({ children }) {
